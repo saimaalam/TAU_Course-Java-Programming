b29e4888510e1ba523e24e2756c45a71e7f8054f
Saima <saimaalam.dsi@gmail.com>
2024-11-26 23:51:25 +0600

    init

--- src/chapter2/MadLibs.java
+++ src/chapter2/MadLibs.java
@@ -3,18 +3,18 @@ package chapter2;
 import java.util.Scanner;
 
 public class MadLibs {
-     public static void main(String[] args) {
-         System.out.println("Enter a season of the year: ");
-         Scanner scanner = new Scanner(System.in);
-         String season= scanner.next();
+    public static void main(String[] args) {
+        System.out.println("Enter a season of the year: ");
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.next();
 
-         System.out.println("Enter a whole number: ");
-         int number= scanner.nextInt();
+        System.out.println("Enter a whole number: ");
+        int number = scanner.nextInt();
 
-         System.out.println("Enter an adjective:");
-         String adjective = scanner.next();
+        System.out.println("Enter an adjective:");
+        String adjective = scanner.next();
 
-         System.out.println("On a "+adjective+" "+season+" day,I drink a minimum of "+number+" cups of coffee.");
+        System.out.println("On a " + adjective + " " + season + " day,I drink a minimum of " + number + " cups of coffee.");
 
-     }
+    }
 }
---
+++ src/chapter3/TestResults.java
@@ -0,0 +1,21 @@
+package chapter3;
+
+import java.util.Scanner;
+
+/*
+ * IF-ELSE-IF
+ * Display the letter grade for a student based on their test score.
+ */
+public class TestResults {
+    public static void main(String args[]){
+
+        //Get the test score
+        System.out.println("Enter your test score:");
+        Scanner scanner = new Scanner(System.in);
+        double score = scanner.nextDouble();
+        scanner.close();
+
+        //Determine the letter grade
+
+    }
+}
