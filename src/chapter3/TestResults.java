cfce905da47e974beb43ce0fe4e7d6a4fc79fd46
Saima <saimaalam.dsi@gmail.com>
2024-11-27 12:57:01 +0600

    init

--- src/chapter3/TestResults.java
+++ src/chapter3/TestResults.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Display the letter grade for a student based on their test score.
  */
 public class TestResults {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         //Get the test score
         System.out.println("Enter your test score:");
@@ -16,6 +16,18 @@ public class TestResults {
         scanner.close();
 
         //Determine the letter grade
-
+        char grade;
+        if (score < 60) {
+            grade = 'F';
+        } else if (score < 70) {
+            grade = 'D';
+        } else if (score < 80) {
+            grade = 'C';
+        } else if (score < 90) {
+            grade = 'B';
+        } else {
+            grade = 'A';
+        }
+        System.out.println("Your grade is: " + grade);
     }
 }
