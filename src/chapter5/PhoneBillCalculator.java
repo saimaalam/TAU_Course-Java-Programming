ecf822cd82bfffdf067f9978a016ffe3b833cd58
Saima <saimaalam.dsi@gmail.com>
2025-04-23 01:09:22 +0600

    init

---
+++ src/chapter5/PhoneBillCalculator.java
@@ -0,0 +1,16 @@
+package chapter5;
+/*
+ * Write an ' Phone bill calculator' program that allow user to input
+ * the plan fee
+ * the number of overage minutes
+ * charge 25 cents for every minute that user have gone over
+ * add 15% tax on the subtotal
+ * Calculate the final total
+ */
+
+public class PhoneBillCalculator {
+    public static void main(String[] args) {
+
+
+    }
+}
