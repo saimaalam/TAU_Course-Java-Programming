11a2b51316d3b070ff5af3e02df9ba8c3b6b49b9
saimaalam <saimaalam.dsi@gmail.com>
2025-04-25 14:30:27 +0600

    init

---
+++ src/chapter6/MonthConverter.java
@@ -0,0 +1,10 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class MonthConverter {
+    public static void main(String[] args) {
+        System.out.println(Month.getMonth(4));
+        System.out.println(Month.getMonth("august"));
+    }
+}
