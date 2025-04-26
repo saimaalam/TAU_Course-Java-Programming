59ff729223adaba949b6277caab10fab016eca5d
Saima <saimaalam.dsi@gmail.com>
2025-04-26 22:25:38 +0600

    init

---
+++ src/chapter7/Grades.java
@@ -0,0 +1,16 @@
+package chapter7;
+
+/*
+ * Create a program that allows a user to enter any
+ * number of grades and provides them with their
+ * average score, as well as the highest and lowest scores.
+ */
+public class Grades {
+    private static int grades[];
+}
+    public static void main(String[] args){
+
+        System.out.println("How many grades would you like to enter?");
+
+        grades = new int[scanner.nextInt()];
+}
