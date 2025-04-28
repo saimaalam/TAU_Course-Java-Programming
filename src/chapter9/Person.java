d3b0a4d393be27699854bced978d50afaade560e
saimaalam <saimaalam.dsi@gmail.com>
2025-04-29 01:58:22 +0600

    init

---
+++ src/chapter9/Person.java
@@ -0,0 +1,28 @@
+package chapter9;
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getGender() {
+        return gender;
+    }
+}
