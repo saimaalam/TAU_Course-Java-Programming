0783192afa7b808829fec48b0c8db89a0b109221
saimaalam <saimaalam.dsi@gmail.com>
2025-05-09 19:35:29 +0600

    init

--- src/chapter9/InheritanceTester.java
+++ src/chapter9/InheritanceTester.java
@@ -4,5 +4,17 @@ public class InheritanceTester {
     public static void main(String[] args) {
         //Person person = new Person();
         Employee employee = new Employee();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("square");
+
+        Mother mom = new Mother();
+        mom.setName("Glenda");
+
+        System.out.println(mom.getName() + " is a " + mom.getGender());
+
     }
 }
